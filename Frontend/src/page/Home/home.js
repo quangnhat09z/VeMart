e2f93a5a1e0f8d3088d52e421495578373b48cbe
quangnhat09z <160835073+quangnhat09z@users.noreply.github.com>
2026-02-26 22:45:16 +0700

    Initial commit

---
+++ Frontend/src/page/Home/home.js
@@ -0,0 +1,134 @@
+
+
+let sliderList = document.querySelectorAll(".slider");
+// console.log(sliderList);
+let length = sliderList.length;
+let flag = 1;
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function nextSlide(n) {
+    flag += n;
+    if (flag <= 0) {
+        flag = length;
+    }
+    else if (flag > length) {
+        flag = 1;
+    }
+    showSlide(flag);
+}
+
+function showSlide(slideIndex) {
+    for (let i = 0; i < length; i++) {
+        sliderList[i].style.display = 'none';
+    }
+    // console.log("Slide Index: " + slideIndex);
+    sliderList[slideIndex - 1].style.display = "block";
+}
+
+
+
+const swiperTrending = new Swiper('.swiper-trending', {
+    direction: 'horizontal', // hoặc 'vertical'
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 5,
+    spaceBetween: 10,
+});
+
+
+const swiperCategory = new Swiper('.swiper-category', {
+    direction: 'horizontal', // hoặc 'vertical'
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 7,
+    spaceBetween: 23,
+});
+
+const swiperProduct = new Swiper('.swiper-products', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 3,
+    spaceBetween: 20,
+});
+
+
+
+
+const swiperComment = new Swiper('.swiper-comment', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+        delay: 10000,
+    },
+
+    slidesPerView: 1,
+});
+
+
+
+
+const swiperNews = new Swiper('.swiper-new', {
+    direction: 'horizontal',
+    loop: true,
+
+    autoplay: {
+        delay: 10000,
+    },
+
+    slidesPerView: 3,
+    spaceBetween: 20,
+});
+
+
+
+const Days = document.querySelector("#days");
+const Hours = document.querySelector("#hours");
+const Minutes = document.querySelector("#minutes");
+const Seconds = document.querySelector("#seconds");
+
+function timer() {
+    let targetDate = new Date("June 1, 2026 00:00:00");
+    let currentDate = new Date();
+    distance = targetDate - currentDate
+
+    let days = Math.floor(distance / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(distance / 1000 / 60 / 60) % 24;
+    let minutes = Math.floor(distance / 1000 / 60) % 60;
+    let seconds = Math.floor(distance / 1000) % 60;
+
+    // console.log("Days:" + days + ", Hours: " + hours + ", Minutes: " + minutes + ", Second" + seconds);
+    Days.innerHTML = days;
+    Hours.innerHTML = hours;
+    Minutes.innerHTML = minutes;
+    Seconds.innerHTML = seconds;
+}
+
+setInterval(timer, 1000);
+
