2a5e2f6286ec39b1140fe3c5f0c2f4a11a366b6a
quangnhat09z <160835073+quangnhat09z@users.noreply.github.com>
2026-03-01 23:47:13 +0700

    show products from database

--- Backend/controllers/client/product.controller.js
+++ Backend/controllers/client/product.controller.js
@@ -4,10 +4,10 @@ module.exports.index = async (req, res) => {
 
     const products = await Product.find({
         status: 'active',
-
     });
     console.log(products);
     res.render("client/pages/product/index", {
         pageTitle: "Danh sách sản phẩm",
+        products: products
     })
 }
