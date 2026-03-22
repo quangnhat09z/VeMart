bda23c1cacb4710d5991bface3de9846e0597c08
quangnhat09z <160835073+quangnhat09z@users.noreply.github.com>
2026-03-22 17:35:38 +0700

    client: section1_FE, show categories

--- Backend/controllers/client/home.controller.js
+++ Backend/controllers/client/home.controller.js
@@ -1,6 +1,17 @@
-// [GET]   /
-module.exports.index = (req, res) => {
+const Category = require("../../models/category.model.js");
+const { buildCategoryHierarchy } = require('../../helpers/categoryTree.js');
+
+// [GET] /
+module.exports.index = async (req, res) => {
+    const filter = {
+        deleted: false
+    };
+    const categories = await Category.find(filter);
+    const categoryHierarchy = buildCategoryHierarchy(categories);
+    console.log(categoryHierarchy);
     res.render("client/pages/home/index", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Home",
+        categories: categories,
+        categoryHierarchy: categoryHierarchy
     })
 }
