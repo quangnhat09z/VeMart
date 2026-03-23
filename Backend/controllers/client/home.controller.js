1ba9c402b672eb152830e698c17577d8c1111aa0
quangnhat09z <160835073+quangnhat09z@users.noreply.github.com>
2026-03-23 19:57:59 +0700

    client: show bestSeller products

--- Backend/controllers/client/home.controller.js
+++ Backend/controllers/client/home.controller.js
@@ -1,17 +1,27 @@
 const Category = require("../../models/category.model.js");
+const Product = require("../../models/product.model.js");
 const { buildCategoryHierarchy } = require('../../helpers/categoryTree.js');
 
 // [GET] /
 module.exports.index = async (req, res) => {
-    const filter = {
-        deleted: false
+    const filterCategory = {
+        deleted: false,
     };
-    const categories = await Category.find(filter);
+    const categories = await Category.find(filterCategory);
     const categoryHierarchy = buildCategoryHierarchy(categories);
-    console.log(categoryHierarchy);
+    
+
+    const filterBestSellerProduct = {
+        deleted : false,
+        isBestSeller: true
+    };
+    const bestSellerProducts = await Product.find(filterBestSellerProduct)
+        .limit(10);
+
     res.render("client/pages/home/index", {
         pageTitle: "Home",
         categories: categories,
-        categoryHierarchy: categoryHierarchy
+        categoryHierarchy: categoryHierarchy,
+        bestSellerProducts: bestSellerProducts
     })
 }
